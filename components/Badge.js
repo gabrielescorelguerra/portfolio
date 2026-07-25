bb7e22f97918f036c26046d499188ed1484e6af8
Gabriel Guerra <gabrielcake9@gmail.com>
2026-07-25 19:39:05 -0300

    adiciona novas tecnologias

--- components/Badge.js
+++ components/Badge.js
@@ -5,11 +5,14 @@ export class Badge extends HTMLElement {
 
     connectedCallback() {
         const icon_class = this.getAttribute("icon_class");
+        const icon_url = this.getAttribute("icon_url");
         const text = this.getAttribute("text");
 
         this.innerHTML = `
-        <div class="text-center">
-            <i class="${icon_class} text-4xl"></i>
+        <div class="flex flex-col justify-between items-center h-full">
+            ${icon_class ? 
+                `<i class="${icon_class} text-4xl"></i>` :
+                `<img src="${icon_url}" alt="Supabase" width="36" class="select-none" disabled>`}
             <p class="text-[0.6rem]">${text}</p>
         </div>
         `;
